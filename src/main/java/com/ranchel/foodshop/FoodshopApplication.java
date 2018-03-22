07a1af74d6145d9c683ebe7fb5fc85c296793e56
ranchel <965505058@qq.com>
2018-03-22 21:17:01 +0800

    Initial commit

---
+++ src/main/java/com/ranchel/foodshop/FoodshopApplication.java
@@ -0,0 +1,12 @@
+package com.ranchel.foodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodshopApplication.class, args);
+	}
+}
